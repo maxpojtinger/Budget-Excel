a5070eca8b1002767be9dc8b09fbb6add342f42c
Max <ge89suw@mytum.de>
2024-06-16 12:33:48 +0200

    push

---
+++ src/main/java/BudgetExcelPackage/TokenType.java
@@ -0,0 +1,18 @@
+package BudgetExcelPackage;
+
+public enum TokenType {
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    LPAREN,
+    RPAREN,
+    ENTRY,
+    POW,
+    GCD,
+    FACTORIAL,
+    COMMA,
+    NUMBER,
+    MODULO,
+    EOF
+}
